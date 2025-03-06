d2f731adba4c702a9fc8749d25787b0492391c57
Sagar <sagarsuma11@gmail.com>
2025-03-06 10:06:59 +0530

    2

---
+++ basic1.java
@@ -0,0 +1,27 @@
+import java.io.*;
+public class basic1
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello world!");
+    }
+}
+/*
+Structure of Java program:
+imports
+
+public class name -> should be same as file name
+{
+    global member data
+    main method
+    {
+        local data
+        logic to execute
+    }
+    external/user defined methods
+}
+other classes
+{
+    no main method allowed
+}
+*/
