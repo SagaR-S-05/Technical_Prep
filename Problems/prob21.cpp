1799f74ed62b354137831bd5a81c7ad367902a7d
Sagar <sagarsuma11@gmail.com>
2025-03-15 13:02:20 +0530

    Edited

--- Problems/prob21.cpp
+++ Problems/prob21.cpp
@@ -23,3 +23,42 @@ int main() {
     }
     return 0;
 }
+/*
+All possible things:
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int findSize(char x[]){
+    int count=0;
+    while(*x!='\0'){
+        x++;
+        count++;
+    }
+    return count;
+}
+void swap(char *x,char *y){
+    char temp=*x;
+    *x=*y;
+    *y=temp;
+}
+void nextPermutation(char x[], int l, int r, int min,int max){
+    if(l==r){
+        if(findSize(x)>=min && findSize(x)<=max)
+        cout<<x<<endl;
+    }else{
+        for(int i=l;i<=r;i++){
+            swap(&x[l],&x[i]);
+            nextPermutation(x,l+1,r,min,max);
+            swap(&x[l],&x[i]);
+        } 
+    }
+}
+int main(){
+    char x[]="abc";
+    int minlen=3;
+    int maxlen=3;
+    nextPermutation(x,0,sizeof(x)-1,minlen,maxlen);    
+    return 0;
+}
+ */
